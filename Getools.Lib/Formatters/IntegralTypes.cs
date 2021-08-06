4ff2c0a5c48c1f6abb539d53c348f999c22d036d
Ben Burns <benjaminaburns@gmail.com>
2021-08-06 17:32:14 -0500

    more documentation

--- Getools.Lib/Formatters/IntegralTypes.cs
+++ Getools.Lib/Formatters/IntegralTypes.cs
@@ -5,8 +5,18 @@ using System.Text;
 
 namespace Getools.Lib.Formatters
 {
+    /// <summary>
+    /// Format converters for integers, pointers, and bytes.
+    /// </summary>
     public static class IntegralTypes
     {
+        /// <summary>
+        /// Converts value as it would be printed in c source.
+        /// Converted to 8-digit hex value, prepended with "0x" if value is not null.
+        /// Otherwise returns "NULL".
+        /// </summary>
+        /// <param name="i">Value to use.</param>
+        /// <returns>Value as hex or "NULL".</returns>
         public static string ToCPointerString(int? i)
         {
             if (i.HasValue)
@@ -18,6 +28,12 @@ namespace Getools.Lib.Formatters
             return "NULL";
         }
 
+        /// <summary>
+        /// Converts array of bytes to c byte array inline declaraction.
+        /// Each value is converted to hex string.
+        /// </summary>
+        /// <param name="bytes">Bytes to use in array.</param>
+        /// <returns>Comma seperated list of values within brackets.</returns>
         public static string ToCInlineByteArray(IEnumerable<byte> bytes)
         {
             var byteString = string.Join(", ", bytes.Select(x => "0x" + x.ToString("x2")));
@@ -25,6 +41,14 @@ namespace Getools.Lib.Formatters
             return "{" + byteString + "}";
         }
 
+        /// <summary>
+        /// Converts single byte to c char inline declaration.
+        /// If the byte is outside the printable ASCII range (32-127 inclusive),
+        /// the byte is returned as hex value.
+        /// </summary>
+        /// <param name="b">Byte to convert.</param>
+        /// <returns>Single quoted byte value if within printable ASCII range,
+        /// otherwise hex value.</returns>
         public static string ByteAsCharStringOrHex(byte b)
         {
             if (b >= 32 && b <= 127)
@@ -37,6 +61,17 @@ namespace Getools.Lib.Formatters
             }
         }
 
+        /// <summary>
+        /// Converts a string to a fixed length array of single characters
+        /// as it would appear in an inline c declaration.
+        /// If the array length exceeds the string length, null characters are
+        /// used to fill the remainder of the array. The string is truncated
+        /// if it exceeds array length, without consideration for terminating
+        /// null character.
+        /// </summary>
+        /// <param name="s">String to convert to char array.</param>
+        /// <param name="arrLen">Length of array to create.</param>
+        /// <returns>Inline c declaration text.</returns>
         public static string StringToCInlineFixedLengthCharArray(string s, int arrLen)
         {
             var bytes = System.Text.Encoding.ASCII.GetBytes(s);
--- Getools.Lib/Formatters/Strings.cs
+++ Getools.Lib/Formatters/Strings.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace Getools.Lib.Formatters
 {
+    /// <summary>
+    /// String format helpers.
+    /// </summary>
     public static class Strings
     {
+        /// <summary>
+        /// Returns the string surrounded with double quotes.
+        /// </summary>
+        /// <param name="s">String to quote.</param>
+        /// <param name="maxLen">
+        /// Optional max length. If value is provided, will truncate
+        /// input string to this length if exceeded.</param>
+        /// <returns>Quoted string.</returns>
         public static string ToQuotedString(string s, int? maxLen = null)
         {
             if (maxLen.HasValue && maxLen >= 0)
